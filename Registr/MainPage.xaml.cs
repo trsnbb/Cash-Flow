b2430653ec65ce14e047a69ddef3d883736b9e40
gelishaa <anhelinamarkus@gmail.com>
2025-04-09 23:56:59 +0300

    #BW-35 - створено .resx файли для локалізації (укр, англ)

---
+++ Registr/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Registr;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage() => InitializeComponent();
+    
+    private void OnConfirmClicked(object sender, EventArgs e)
+    {
+        var email = EmailEntry.Text;
+        var password = PasswordEntry.Text;
+
+        // TODO: Додати логіку реєстрації
+        DisplayAlert("Інфо", $"Email: {email}\nPassword: {password}", "OK");
+    }
+
+    private void OnGoogleLoginClicked(object sender, EventArgs e)
+    {
+        // TODO: Авторизація через Google
+        DisplayAlert("Google", "Google авторизація", "OK");
+    }
+
+    private void OnFacebookLoginClicked(object sender, EventArgs e)
+    {
+        // TODO: Авторизація через Facebook
+        DisplayAlert("Facebook", "Facebook авторизація", "OK");
+    }
+}
---
+++ Registr/MauiProgram.cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+namespace Registr
+{
+    public static class MauiProgram
+    {
+        public static IncomingRepository IncomingRepository { get; private set; }
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Regular.ttf", "Montserrat");
+                    fonts.AddFont("Montserrat-Bold.ttf", "MontserratBold");
+
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            SetCultureInfo();
+            ConfigDb();
+
+            return builder.Build();
+        }
+
+        private static void SetCultureInfo()
+        {
+            CultureInfo culture = new CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+
+        private static void ConfigDb()
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "cach-flow.db3");
+            IncomingRepository = new IncomingRepository(dbPath);
+        }
+    }
+}
